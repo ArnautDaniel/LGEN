30b8b8e189c925cd472f67eb8af36acf67433590
Silverbeard00 <silverbeard@protonmail.com>
2016-08-26 14:38:57 -0400

    Fixed Schema to add date and contact info

--- server.rkt
+++ server.rkt
@@ -8,12 +8,21 @@
          web-server/servlet
          web-server/configuration/responders
          web-server/servlet-env
-         db)
+         db
+         racket/date)
 
 (provide/contract (start (request? . -> . response?)))
+
 (define lgen-path "/home/silver/Desktop/lgen/lgen.db")
+
 (define db (sqlite3-connect #:database lgen-path #:mode 'create))
 
+(define dateToday (string-join
+                   (map number->string
+                        (list
+                         (date-month (current-date))
+                         (date-day (current-date))
+                         (date-year (current-date)))) "-"))
 (define (clean-show-name a-showname)
   (string-join (string-split a-showname " ") ""))
 
@@ -23,7 +32,7 @@
   (query-exec db
               (string-append
                "CREATE TABLE " (clean-show-name (invoice-show a-invoice))
-               " (id INTEGER PRIMARY KEY, nameSet TEXT, item TEXT, price INTEGER, qty INTEGER)")))
+               " (id INTEGER PRIMARY KEY, nameSet TEXT, item TEXT, price INTEGER, qty INTEGER, contact TEXT, todaysDay TEXT, checkd TEXT)")))
 
 (define (insert-invoice-data! lgen-path a-invoice)
   (define (insert-bodylist-data! a-bodylist)
@@ -34,11 +43,14 @@
        (query-exec db
                    (string-append
                     "INSERT INTO " (clean-show-name (invoice-show a-invoice))
-                    "(nameSet, item, price, qty) VALUES (?, ?, ?, ?)")
+                    "(nameSet, item, price, qty, contact, todaysDay, checkd) VALUES (?, ?, ?, ?, ?, ?, ?)")
                    (invoice-set a-invoice)
                    (body-description (car a-bodylist))
                    (body-price (car a-bodylist))
-                   (body-qty (car a-bodylist)))
+                   (body-qty (car a-bodylist))
+                   (invoice-person a-invoice)
+                   dateToday
+                   "false")
        (insert-bodylist-data! (cdr a-bodylist)))))
     
   (unless (table-exists? db (clean-show-name (invoice-show a-invoice)))
